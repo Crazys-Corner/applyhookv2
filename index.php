841cc70b1b07bbad96d4e460a34864887d9aca82
crazys-corner <craazygaming1021@gmail.com>
2023-09-15 12:16:13 -0400

    Cards (WIP)

--- index.php
+++ index.php
@@ -35,10 +35,34 @@
         </ul>
     </nav>
     <h1 class="text-center">Applications</h1>
+    <div class="d-flex justify-content-between">
+    <div class="card col-12 col-sm-6 col-md-4 col-lg-3" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card col-12 col-sm-6 col-md-4 col-lg-3" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card col-12 col-sm-6 col-md-4 col-lg-3" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
 
-  
-
-
+</div>
     <script>
     // Function to toggle dark mode
     function toggleDarkMode() {
